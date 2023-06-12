6d9864153480a442468f5d07d1b55b695df30052
ImmortalDev <34840787+SikanaAli@users.noreply.github.com>
2023-06-12 11:26:12 +0200

    day 12 project complete

--- Day_12/main.py
+++ Day_12/main.py
@@ -7,12 +7,13 @@ print(logo)
 
 # player prefs
 lives = 0
-difficaulty = ""
 
+# Game Prefs
+guess_is_correct = False
 
 # init
 # prints welcome message and asks the user
-# to select a dificulty
+# to select a difficulty
 def init():
     init_msg = f"""
         Welcome to the number guesser
@@ -21,12 +22,50 @@ def init():
     """
     print(init_msg)
 
-    return input("Choose a dificulty. type 'easy' or 'hard': ").lower()
+    return input("Choose a difficulty. type 'easy' or 'hard': ").lower()
 
 
-#def validate_guess(guess):
+def validate_guess(player_guess, correct_guess):
+    global guess_is_correct
+    global lives
+    if player_guess > correct_guess:
+        print("Too High.\nGuess Again.")
+        lives -= 1
+    elif player_guess < correct_guess:
+        print("Too Low.\nGuess Again.")
+        lives -= 1
+    elif player_guess == correct_guess:
+        print(f"You win, the correct guess was {correct_guess}")
+        guess_is_correct = True
+
+    if lives < 1:
+        print(f"Attempts {lives}, you are out of attempts, You Loose")
+        guess_is_correct = True
 
 
-#def number_guesser():
+difficulty = init()
 
-init()
+
+def set_lives():
+    global lives
+    if difficulty == "easy":
+        lives = 10
+    else:
+        lives = 5
+
+
+set_lives()
+
+
+def number_guesser():
+
+    # randomly generated number
+    correct_number = random.randint(1, 100)
+
+    while not guess_is_correct:
+        print(f"You have {lives} attempts remaining to guess the number.")
+        guess = int(input("Make a Guess: "))
+        validate_guess(guess, correct_number)
+
+
+number_guesser()
