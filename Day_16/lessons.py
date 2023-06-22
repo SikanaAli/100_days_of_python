f8dfc0e74d01c11531c4f31d6a117c61aa4a565a
ImmortalDev <34840787+SikanaAli@users.noreply.github.com>
2023-06-22 15:32:55 +0200

    day 16 project done

---
+++ Day_16/lessons.py
@@ -0,0 +1,26 @@
+# from turtle import Turtle, Screen
+#
+# pawpaw = Turtle()
+#
+# pawpaw.shape("turtle")
+# pawpaw.color("coral")
+# pawpaw.forward(100)
+#
+# my_screen = Screen()
+#
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+
+p_names = ["Pikachu","Charmander","Squirtle"]
+p_type = ["lightning", "fire", "water"]
+
+table.add_column("Pokemon Name", p_names)
+table.add_column("Type", p_type)
+
+table.align = "l"
+
+print(table)
--- Day_16/main.py
+++ Day_16/main.py
@@ -1,26 +1,35 @@
-# from turtle import Turtle, Screen
-#
-# pawpaw = Turtle()
-#
-# pawpaw.shape("turtle")
-# pawpaw.color("coral")
-# pawpaw.forward(100)
-#
-# my_screen = Screen()
-#
-# my_screen.exitonclick()
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-from prettytable import PrettyTable
+coffee_machine = CoffeeMaker()
+money_counter = MoneyMachine()
+coffee_menu = Menu()
 
-table = PrettyTable()
+is_on = True
 
 
-p_names = ["Pikachu","Charmander","Squirtle"]
-p_type = ["lightning", "fire", "water"]
+def check_choice(choice):
+    match choice:
+        case "report":
+            coffee_machine.report()
+            money_counter.report()
+            return
+        case "latte" | "espresso" | "cappuccino":
+            drink_ordered = coffee_menu.find_drink(choice)
 
-table.add_column("Pokemon Name", p_names)
-table.add_column("Type", p_type)
+            have_resource = coffee_machine.is_resource_sufficient(drink_ordered)
+            if have_resource:
+                money_enough = money_counter.make_payment(drink_ordered.cost)
+                if money_enough:
+                    coffee_machine.make_coffee(drink_ordered)
+            return
+        case _:
+            global is_on
+            is_on = False
+            return
 
-table.align = "l"
 
-print(table)
+while is_on:
+    user_choice = input(f"What would you like? {coffee_menu.get_items()}: ")
+    check_choice(user_choice)
